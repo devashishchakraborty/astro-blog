49274fa839e4902a85121e22a56ade3f25f0c68d
Devashish Chakraborty <devashishchakra@gmail.com>
2025-06-20 06:55:45 +0530

    Completed the astro tutorial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-blog-4ea.pages.dev/",
+  integrations: [react()],
 });
